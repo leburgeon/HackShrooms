0131c3a47c3854e3104caedf1e38190d4448eee1
Kieran <k1phelan234@gmail.com>
2025-12-22 17:13:19 +0000

    Dynamically adding upgrades

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 import helper from "./helper";
+
 //initial number of cookies    
 var num = 0;
 
@@ -46,3 +47,80 @@ function mushroomClick() {
     }
 }
 
+const autoUpgrades = [
+    {
+        name: "Grow Bed",
+        lvl: 0,
+        cost: 10
+    },
+    {
+        name: "Farm",
+        lvl: 0,
+        cost: 100
+    },
+    {
+        name: "Laboratory",
+        lvl: 0,
+        cost: 1000
+    }
+]
+
+const clickerUpgrades = [
+    {
+        name: "Scythe",
+        lvl: 0,
+        cost: 10
+    },
+    {
+        name: "Tractor",
+        lvl: 0,
+        cost: 100
+    }
+]
+
+function buildUpgradeCard(upgrade) {
+    const card = document.createElement("div");
+    card.classList = "card";
+
+    const content = `
+    <div class="card">
+        <div class="card-body">
+            <div class="container text-center">
+                <div class="row align-items-start">
+                    <div class="col">
+                        (image)
+                    </div>
+                    <div class="col">
+                        ${upgrade.name}
+                    </div>
+                    <div class="col">
+                        ${upgrade.lvl}
+                    </div>
+                    <div class="col">
+                        (button) ${upgrade.cost}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    return content
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("page loaded")
+
+    const autoUpgradeContainer = document.getElementById("auto-upgrades-list");
+    
+    autoUpgrades.forEach((upgrade) => {
+        content = buildUpgradeCard(upgrade)
+        autoUpgradeContainer.innerHTML += content;
+    })
+
+    const clickerUpgradeContainer = document.getElementById("clicker-upgrades-list");
+    clickerUpgrades.forEach((upgrade) => {
+        content = buildUpgradeCard(upgrade)
+        clickerUpgradeContainer.innerHTML += content;
+    })
+})
