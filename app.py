1d88c55e33fe3ee23f2f0c4b63da02b884c9e3d1
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-12-22 15:39:18 +0000

    feat: Add player load and save utility to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from utils import save_player_data, load_player_data, add_new_user
+from utils import save_player_data, load_player_data, save_player_data
 
 
 app = Flask(__name__)
@@ -15,8 +15,15 @@ def savedata():
     """ Saves player data to the database """
     player_data = request.json
     save_player_data(player_data)
+    return "Player data successfully uploaded", 200
 
 
-@app.route('/getplayerdata', methods=['GET'])
-def getplayerdata():
+@app.route('/getplayerdata/<string:username>', methods=['GET'])
+def getplayerdata(username):
     """ Retrieves player data from the database """
+    player_data = load_player_data(username)
+    return player_data, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
