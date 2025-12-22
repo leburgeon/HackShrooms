d283bfaccd9c5f585978ec0ed1ab53fb48050d96
leburgeon <trainee.lewis.burgess@sigmalabs.co.uk>
2025-12-22 15:20:23 +0000

    Some flask work on endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from utils import save_player_data, load_player_data, add_new_user
 
 
 app = Flask(__name__)
@@ -7,3 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/saveplayerdata', methods=['POST'])
+def savedata():
+    """ Saves player data to the database """
+    player_data = request.json
+    save_player_data(player_data)
+
+
+@app.route('/getplayerdata', methods=['GET'])
+def getplayerdata():
+    """ Retrieves player data from the database """
