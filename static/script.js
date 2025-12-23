9af93eedb317ac810f277eb35b041e7a7003451d
Kieran <k1phelan234@gmail.com>
2025-12-23 15:29:47 +0000

    Add more auto generation objects

--- static/script.js
+++ static/script.js
@@ -1,47 +1,63 @@
 
 
-//initial number of cookies    
+//initial number of cookies
 var Game = {
     mushrooms: 0,
     lifetimeMushrooms: 0,
     upgrades: {
         auto: [
-        {
-            name: "Grow Bed",
-            lvl: 0,
-            baseCost: 10,
-            cost: 10,
-            baseValue: 1
-        },
-        {
-            name: "Farm",
-            lvl: 0,
-            baseCost: 100,
-            cost: 100,
-            baseValue: 10
-        },
-        {
-            name: "Laboratory",
-            lvl: 0,
-            baseCost: 1000,
-            cost: 1000,
-            baseValue: 100
-        }],
+            {
+                name: "Grow Bed",
+                lvl: 0,
+                baseCost: 10,
+                cost: 10,
+                baseValue: 1
+            },
+            {
+                name: "Terrarium",
+                lvl: 0,
+                baseCost: 100,
+                cost: 100,
+                baseValue: 5
+            },
+            {
+                name: "Greenhouse",
+                lvl: 0,
+                baseCost: 1000,
+                cost: 1000,
+                baseValue: 10
+            },
+            {
+                name: "Farm",
+                lvl: 0,
+                baseCost: 10000,
+                cost: 10000,
+                baseValue: 500
+            },
+            {
+                name: "Laboratory",
+                lvl: 0,
+                baseCost: 100000,
+                cost: 100000,
+                baseValue: 1000
+            }
+        ],
         clicker: [
-        {
-            name: "Scythe",
-            lvl: 0,
-            baseCost: 10,
-            cost: 10,
-            baseValue: 1
-        },
-        {
-            name: "Tractor",
-            lvl: 0,
-            baseCost: 100,
-            cost: 100,
-            baseValue: 5
-        }]
+            {
+                name: "Scythe",
+                lvl: 0,
+                baseCost: 10,
+                cost: 10,
+                baseValue: 1
+            },
+            {
+                name: "Tractor",
+                lvl: 0,
+                baseCost: 100,
+                cost: 100,
+                baseValue: 5
+            }
+        ]
     }
 }
 
@@ -157,7 +173,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("page loaded")
 
     const upgradeImages = {
-    "auto": [images.bed, images.farm, images.lab],
+    "auto": [images.bed, images.terrarium, images.greenhouse, images.farm, images.lab],
     "clicker": [images.scythe, images.tractor]
     }
     // Load player data if any here
